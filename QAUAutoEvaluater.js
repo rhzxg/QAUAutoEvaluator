9240f7d6da82dcedb6511e9a88c73f448d9f038c
郑相关 <codingdogzxg@gmail.com>
2021-11-13 16:22:14 +0800

    Update QAUAutoEvaluater.js

--- QAUAutoEvaluater.js
+++ QAUAutoEvaluater.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         青岛农业大学自动评教脚本
 // @namespace    https://github.com/CodingDogzxg
-// @version      0.3 beta 2021/8/7 built
+// @version      0.3.2 beta 2021/11/13 built
 // @description  青岛农业大学 QAU 期末评教 自动评教
 // @author       QAUCodingDog_zxg
 // @include      http://jwglxt.qau.edu.cn/jsxsd1/xspj/xspj_find.do?*
@@ -10,21 +10,21 @@
 // @grant        GM_addStyle
 // @license      MIT
 // ==/UserScript==
-
-
+ 
+ 
 // 变量
 var url = location.pathname,
 mainPage = "http://jwglxt.qau.edu.cn/jsxsd1/xspj/xspj_find.do",
 evaluationInfoPage = "http://jwglxt.qau.edu.cn/jsxsd1/xspj/xspj_list.do",
 evaluationMainPage = "http://jwglxt.qau.edu.cn/jsxsd1/xspj/xspj_edit.do";
-
+ 
 // 绘制窗口
 if (mainPage.indexOf(url) != -1) {
     // 主题
     var style_btn = 'float:right;background:rgba(228,228,228,0.4); cursor:pointer; margin:0px 1px 0px 0px; padding:0px 3px;color:black; border:2px ridge black;border:2px groove black;';
     var style_win_top = 'z-index:998; padding:6px 10px 8px 15px;background-color:lightGrey;position:fixed;left:5px;top:5px;border:1px solid grey; ';
     var style_win_buttom = 'z-index:998; padding:6px 10px 8px 15px;background-color:lightGrey;position:fixed;right:5px;bottom:5px;border:1px solid grey;  ';
-
+ 
     // 开始绘制
     var newDiv = document.createElement("div");
     newDiv.id = "controlWindow";
@@ -76,51 +76,51 @@ if (mainPage.indexOf(url) != -1) {
     td2.appendChild(open);
     GM_addStyle("#open{" + style_btn + "}");
 }
-
+ 
 function StartEvaluate() {
     // 评教页面第一页 显示课程分类
     var tableBox = document.getElementsByClassName("Nsb_r_list Nsb_table");
     var tableList = tableBox[0].getElementsByTagName("a");
-
+ 
     // window.open() 非用户发出会被浏览器阻挡 延时则不会
     // setTimeout(window.open("http://www.baidu.com", "_blank"), 500);
-
+ 
     var pageList = [];
-    for(var i=0;i<tableList.length;i++){
+    for(var i = 0;i < tableList.length;i++){
         pageList.push(tableList[i].getAttribute("href"));
     }
-    for(var j=0; j<pageList.length; j++) {
+    for(var j = 0; j < pageList.length; j++) {
         window.open(pageList[j], "_blank");
-    };
+    }
 }
-
+ 
 // 评教页面第二页 显示老师
 if (evaluationInfoPage.indexOf(url) != -1) {
     console.log(1);
     var teacherTableBox = document.getElementsByClassName("Nsb_r_list Nsb_table");
     var teacherTableList = teacherTableBox[0].getElementsByTagName("a");
     // /\'(.*)\'/.exec(c)[0].slice(1, -1)
-
+ 
     var teacherPageList = [];
     for(var t_i=0;t_i<teacherTableList.length;t_i++){
         teacherPageList.push(/\'(.*)\'/.exec(teacherTableList[t_i].getAttribute("href"))[0].slice(1, -11));
-    };
-    console.log(teacherPageList[0]);
-    for(var t_j=0; t_j<teacherPageList.length; t_j++) {
+    }
+    // console.log(teacherPageList[0]);
+    for(var t_j = 0; t_j < teacherPageList.length; t_j++) {
         window.open(teacherPageList[t_j], "_blank");
-    };
+    }
 }
-
+ 
 // 评教页面第三页 等级判断 保存按钮
 if (evaluationMainPage.indexOf(url) != -1) {
     var allTable = document.getElementById("table1").getElementsByTagName("tr");
     var allTableLen = allTable.length;
-    for(var k=1; k<allTableLen; k++){
+    for(var k = 1; k < allTableLen; k++){
         if(k != allTableLen - 1){
-        allTable[k].getElementsByTagName("td")[1].getElementsByTagName("input")[0].setAttribute("checked", "checked");
+            allTable[k].getElementsByTagName("td")[1].getElementsByTagName("input")[0].setAttribute("checked", "checked");
         }
         else{
-        allTable[k].getElementsByTagName("td")[1].getElementsByTagName("input")[2].setAttribute("checked", "checked")
+            allTable[k].getElementsByTagName("td")[1].getElementsByTagName("input")[2].setAttribute("checked", "checked");
         }
     }
     var saveButton = document.getElementById("bc");
